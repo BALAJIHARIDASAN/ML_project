5dcd588d90584be06791f2f0943d39677bc9dae6
Balaji Haridasan <balajiharidasan@gmail.com>
2023-06-05 20:23:26 +0530

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,17 @@
 
 
 from setuptools import find_packages,setup
+from typing import List
 
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->list[str]:
+    reqirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.repleace('\n',"") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 
 setup(name = 'mlproject',
@@ -10,5 +20,5 @@ setup(name = 'mlproject',
       author = 'balaji',
       author_email = 'balajiharidasan@gmail.com',
       packages=find_packages(),  # this line will see where there is the __init__ and install it
-      install_requires = ['pandas','numpy','seaborn'])
+      install_requires = get_requirements('requirements.txt'))
 
